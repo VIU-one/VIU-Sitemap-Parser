8425bfa24369fa6b46ed063794b77c1ca2206673
michael <michael@searchviu.com>
2021-07-27 21:28:17 +0200

    Setup Pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from distutils.core import setup
 import os
 
+version = os.environ.get('RELEASE_NUMBER', None)
+
+if not version:
+    version = os.environ.get('RUN_ID', '0.1')
+
 setup(
     name='VIU Sitemap parser',
-    version=os.environ.get('RELEASE_NUMBER', '0.1'),
+    version=version,
     packages=['viu-sitemap-parser'],
     license='MIT',
     long_description=open('README.md').read(),
