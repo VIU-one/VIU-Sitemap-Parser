9cccf08076681d318507e82219584e02ee919dfb
michael <michael@searchviu.com>
2021-07-27 21:12:28 +0200

    Setup Pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
+import os
 
 setup(
     name='VIU Sitemap parser',
-    version='0.1',
-    packages=['viusitemapparser'],
+    version=os.environ.get('RELEASE_NUMBER', '0.1'),
+    packages=['viu-sitemap-parser'],
     license='MIT',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown"
