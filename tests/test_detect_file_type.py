cba326e34af1e89ec4131fbd62607610ec278d67
michael <michael@searchviu.com>
2021-07-28 13:50:16 +0200

    Add tests

--- tests/test_detect_file_type.py
+++ tests/test_detect_file_type.py
@@ -17,3 +17,6 @@ def test_detect_filetype():
 
     sitemap = process_sitemap('tests/sitemap_types/rss_feed.xml')
     assert sitemap.sitemap_type == 'rss_feed'
+
+    sitemap = process_sitemap('tests/sitemap_types/xml_invalid.xml')
+    assert sitemap.sitemap_type == 'invalid_file_format'
---
+++ tests/test_url_functions.py
@@ -0,0 +1,13 @@
+from viusitemapparser.url_functions import check_if_url
+
+
+def test_url_functions():
+    assert check_if_url('https://www.test.de') is True
+    assert check_if_url('https://www.test.de/path') is True
+    assert check_if_url('https://www.test.de/?param=value') is True
+    assert check_if_url('https://www.test.de/?param=') is True
+    assert check_if_url('http://www.test1234.com') is True
+    assert check_if_url('www.test1234.com') is False
+    assert check_if_url('/folder') is False
+    assert check_if_url('/folder/folder') is False
+    assert check_if_url('/folder/folder/folder') is False
